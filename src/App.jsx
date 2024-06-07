9309a356889cb7eb950a889ed595762bff523649
JC Park <whdekf@naver.com>
2024-06-07 18:40:50 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <main>
+            <h1>Let&#39;s play Tic-Tac-Toe!</h1>
+            <header>
+                <ul>
+                    <li>Player 1: X</li>
+                    <li>Player 2: O</li>
+                </ul>
+            </header>
+            <section>
+
+            </section>
+        </main>
+    </>
+  )
+}
+
+export default App
