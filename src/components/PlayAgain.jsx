2ab5c430f120635bf71dcd83d2963a47613fc980
JC Park <whdekf@naver.com>
2024-06-29 00:03:47 +0900

    fix : proper function style

--- src/components/PlayAgain.jsx
+++ src/components/PlayAgain.jsx
@@ -1,12 +1,9 @@
-export default function PlayAgain (winner, onPlayAgain){
+export default function PlayAgain ({winner, onPlayAgain}){
     return (
         <div className="text-center">
             {winner !== null && <button
                 className="bg-orange-600 border-2 border-black text-white rounded-lg px-2 py-1 mt-4"
-                onClick={() => {
-                    onPlayAgain([]);
-                }}
-            >
+                onClick={() => onPlayAgain()}>
                 Play Again
             </button>}
         </div>
